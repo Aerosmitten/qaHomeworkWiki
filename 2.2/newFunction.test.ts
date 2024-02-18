dcb638681da1c710843b9bd3e89e6e5b2a8c26c7
Aerosmitten <139496075+Aerosmitten@users.noreply.github.com>
2024-02-18 13:30:41 -0700

    More changes to homework

--- 2.2/newFunction.test.ts
+++ 2.2/newFunction.test.ts
@@ -1,27 +1,27 @@
 //!Here we are importing our function called myFunciton from our newFunction.ts file so we can use it in our test.
 import { myFunction } from "./newFunction";
 
-describe("myFunction", () => {
+    describe("myFunction", () => {
   it("returns the word 'true' if I pass the function the number 5", () => {
-    expect(myFunction(5)).toBe("true");
-  });
+      expect(myFunction(5)).toBe("true");
+    });
   it("returns the word 'false' if I pass in a number less than 5", () => {
-    expect(myFunction(1)).toBe("false");
-  });
+expect(myFunction(1)).toBe("false");
+    });
   it("returns the word 'big' if I pass in a numbewr greater than 5", () => {
-    expect(myFunction(100)).toBe("big");
-  });
+      expect(myFunction(100)).toBe("big");
+    });
   it("returns the word 'negative' if a negative number is passed in", () => {
     expect(myFunction(-1)).toBe("negative");
   });
 });
-
+  
 
 import { blockBuster } from "./newFunction";
 
 describe('Testing Block Buster', () => {
   it('should return the movie set for MovieOne', () => {
-    expect(blockBuster("Movie One")).toBe("Free Guy"); 
+    expect(blockBuster('Movie One')).toBe("Free Guy")
   }); 
   it("should return the movie set for movie two", () => {
     expect(blockBuster('Movie Two')).toBe("Everything, Everywhere, All at Once"); 
